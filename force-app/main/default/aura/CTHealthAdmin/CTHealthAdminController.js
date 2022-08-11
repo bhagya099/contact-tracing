44b0c0de0509d64e77c5a54e5f96871f55336ca9
Bhagyashree <bhagya099@gmail.com>
2022-08-11 14:08:57 +1000

    making aheader and tab component and adding some css in lightning component

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -1,5 +1,11 @@
 ({
-    myAction : function(component, event, helper) {
-
+  tabSelectHandler: function (component, event, helper) {
+    const selectedTabID = event.getParam("id");
+    if (selectedTabID === "person_view") {
+      component.set("v.headerTitel", "Person View");
+    } else {
+      component.set("v.headerTitel", "Location View");
     }
-})
+    component.set("v.scope", selectedTabID);
+  }
+});
--- force-app/main/default/triggers/CTPersonTrigger.trigger
+++ force-app/main/default/triggers/CTPersonTrigger.trigger
@@ -2,6 +2,7 @@ trigger CTPersonTrigger on Person__c (before insert, before update, before delet
                                     after insert, after update,  after delete, after undelete  ) {
     switch on Trigger.operationType {
         when  BEFORE_INSERT {
+            
             CTPersonTriggerHandler.beforeInsert(Trigger.new);
 
         }
